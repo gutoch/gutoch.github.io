ca38287bc0ad3174a3219f5df80fd4e3ade0fad2
eltongutoch <63658623+eltongutoch@users.noreply.github.com>
2020-12-15 10:12:20 +0100

    V3

---
+++ script.js
@@ -0,0 +1,4 @@
+function instructions () {
+    window.alert("Use your mouse or touchpad to navigate through the exibition. Zoom in an out for details and to look for hidden messages. The page will ask to access your webcam for visual effects. No images will be recorded.")
+    console.log("It's all gonna be fine.")
+}
--- welcome.js
+++
@@ -1,4 +0,0 @@
-function instructions () {
-    window.alert("Use your mouse or touchpad to navigate through the exibition. Zoom in an out for details and to look for hidden messages.")
-    console.log("It's all gonna be fine.")
-}
