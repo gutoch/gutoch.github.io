17f0d8127336de8e448d2ca49e38b34235d852ec
eltongutoch <63658623+eltongutoch@users.noreply.github.com>
2020-12-28 16:30:19 +0100

    New-Alert

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function instructions () {
-    window.alert("Use your mouse or touchpad to navigate through the exibition. Zoom in an out for details and to look for hidden messages. The page will ask to access your webcam for visual effects. No images will be recorded.")
+    window.alert("📌 Instructions:  🔵 Click and drag with your mouse or touchpad to navigate through the exhibition.  🟣 Zoom in an out for smaller details and to look for hidden messages.  🔴 The page will ask to access your webcam for visual effects only. No images will be recorded.  🎧 Use headphones for better experience.")
     console.log("It's all gonna be fine.")
 }
